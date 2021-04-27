8a03a5bfa600a269df48fda7f6857bb89818231a
cruwaller <cruwaller@gmail.com>
2021-04-28 00:28:07 +0300

    more comments added

--- src/src/stm32/platform.cpp
+++ src/src/stm32/platform.cpp
@@ -1,24 +1,30 @@
-#if defined(PLATFORM_STM32)
-
 #include <stm32_def.h>
 #include <string.h>
 #include <stdint.h>
 
-void copy_functions_to_ram(void)
+static void copy_functions_to_ram(void)
 {
-    /* Load functions into RAM */
+    /* Copy RAM_CODE section into RAM.
+     * NOTE: vectors are the first and then other code.
+     *       Check variants/ldscript_gen.ld file.
+     */
     extern uint8_t ram_code_start;
     extern uint8_t ram_code_end;
     extern uint8_t ram_code;
     memcpy(&ram_code_start, &ram_code, (size_t) (&ram_code_end - &ram_code_start));
 }
 
-
-
-__attribute__((constructor(102))) void perkele_initVariant(void)
+__attribute__((constructor(102))) void init_vectors(void)
 {
     copy_functions_to_ram();
-    /* Reset vector location which is set wrongly by SystemInit */
+
+    /** Reset vector location
+     *
+     * g_pfnVectors variable comes from the system startup files and
+     * must always point to beginning of the vectors table where
+     * stack pointer is the first word and second is the reset vector.
+     * Vector table is automatically aligned by the linker command file.
+     */
     extern uint32_t g_pfnVectors;
     SCB->VTOR = (__IO uint32_t) &g_pfnVectors;
 }
@@ -28,5 +34,3 @@ void initVariant(void)
 {
     /* This can be used to run some init before \ref setup() */
 }
-
-#endif /* PLATFORM_STM32 */
